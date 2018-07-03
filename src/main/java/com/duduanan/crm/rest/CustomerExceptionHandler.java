c1f808f69b6bea98b6da7a54f90551eec63ad715
Jerry Guo <jerryguowei@gmail.com>
2018-07-03 21:19:27 +1000

    adding error exception handel for api/customer/id

---
+++ src/main/java/com/duduanan/crm/rest/CustomerExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.duduanan.crm.rest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class CustomerExceptionHandler {
+	@ExceptionHandler
+	public ResponseEntity<CustomerErrorResponse> handleException(CustomerNotFoundException ex){
+		CustomerErrorResponse error=new CustomerErrorResponse();
+		error.setStatus(HttpStatus.NOT_FOUND.value());
+		error.setMessage(ex.getMessage());
+		error.setTimeStamp(System.currentTimeMillis());
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);		
+	}
+	@ExceptionHandler
+	public ResponseEntity<CustomerErrorResponse> handleException(Exception ex){
+		CustomerErrorResponse error=new CustomerErrorResponse();
+		error.setStatus(HttpStatus.BAD_REQUEST.value());
+		error.setMessage(ex.getMessage());
+		error.setTimeStamp(System.currentTimeMillis());
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);		
+	}
+}
--- src/main/java/com/duduanan/crm/rest/CustomerRestController.java
+++ src/main/java/com/duduanan/crm/rest/CustomerRestController.java
@@ -3,6 +3,9 @@ package com.duduanan.crm.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,8 +25,11 @@ public class CustomerRestController {
 	}
 	@GetMapping("/customers/{customerId}")
 	public Customer customer(@PathVariable int customerId){
-		
-		return customerservice.getCustomer(customerId);
+		Customer customer=customerservice.getCustomer(customerId);
+		if(customer==null){
+			throw new CustomerNotFoundException("customer id not found - "+ customerId);
+		}
+		return customer;
 	}
-
+	
 }
