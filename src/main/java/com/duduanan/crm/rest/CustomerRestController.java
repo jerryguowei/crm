1cc15b92aa797a7a32c85e1c22c2ec975f0b4fb1
Jerry Guo <jerryguowei@gmail.com>
2018-07-03 21:42:16 +1000

    add rest create customer api

--- src/main/java/com/duduanan/crm/rest/CustomerRestController.java
+++ src/main/java/com/duduanan/crm/rest/CustomerRestController.java
@@ -1,13 +1,13 @@
 package com.duduanan.crm.rest;
 
 import java.util.List;
+import java.util.function.Function;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,12 +23,25 @@ public class CustomerRestController {
 		
 		return  customerservice.getCustomers();
 	}
+	
 	@GetMapping("/customers/{customerId}")
 	public Customer customer(@PathVariable int customerId){
 		Customer customer=customerservice.getCustomer(customerId);
 		if(customer==null){
 			throw new CustomerNotFoundException("customer id not found - "+ customerId);
 		}
+		return customer;
+	}
+	/**
+	 * Create Customer API
+	 * Method:POST
+	 * @param customer
+	 * @return Customer
+	 */
+	  @PostMapping("customers")
+	public Customer customer(@RequestBody Customer customer){
+		customer.setId(0);
+		customerservice.saveCustomer(customer);	
 		return customer;
 	}
 	
