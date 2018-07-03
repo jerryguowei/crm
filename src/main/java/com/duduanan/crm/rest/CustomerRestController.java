08e639008c1f910a746c891c93ed38b511124980
Jerry Guo <jerryguowei@gmail.com>
2018-07-03 22:01:28 +1000

    adding rest update and delete

--- src/main/java/com/duduanan/crm/rest/CustomerRestController.java
+++ src/main/java/com/duduanan/crm/rest/CustomerRestController.java
@@ -4,9 +4,11 @@ import java.util.List;
 import java.util.function.Function;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -38,11 +40,40 @@ public class CustomerRestController {
 	 * @param customer
 	 * @return Customer
 	 */
-	  @PostMapping("customers")
+	  @PostMapping("/customers")
 	public Customer customer(@RequestBody Customer customer){
 		customer.setId(0);
 		customerservice.saveCustomer(customer);	
 		return customer;
 	}
+	 
+	  /**
+		 * Update Customer API
+		 * Method:PUT
+		 * @param customer
+		 * @return Customer
+		 */
+		  @PutMapping("/customers")
+		public Customer updateCustomer(@RequestBody Customer customer){
+			if(customer.getId()==0){
+			 throw new RuntimeException("customer id is required!");
+			}
+			customerservice.saveCustomer(customer);	
+			return customer;
+		}
+		  
+		 @DeleteMapping("/customers/{customerId}")
+		 public String deleteCustomer(@PathVariable int customerId){
+			 Customer tempCustomer=customerservice.getCustomer(customerId);
+			 if(tempCustomer==null){
+				 throw new CustomerNotFoundException("customer not found - " + customerId);
+			 }
+			 customerservice.deleteCustomer(customerId);
+			 if(customerservice.getCustomer(customerId)==null){
+				 return "delete customer with id " + customerId;
+			 }else{
+				 return "failed to delte customer with id " + customerId;
+			 }	
+		 }
 	
 }
